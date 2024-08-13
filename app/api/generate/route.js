9b0a876e777922b3e90e614773d061f465e801ac
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-08-13 18:50:35 -0400

    Added functionality to prompt Gemini.
    Added functionality to prompt Gemini.

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,22 +1,21 @@
 import { NextResponse } from "next/server";
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Set your API key here or use the environment variable
-const genAI = new GoogleGenerativeAI({ apiKey: process.env.GEMINI_API_KEY });
-
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
 
 const systemInstruction = `
 You are a flash card Generator. Your task is to generate concise and effective flashcards based on the given topic or content.
 1. Create clear and concise flashcards.
 2. Provide accurate and relevant information on the back of the flash cards.
-3. Ensure that each flash card focuses on a piece of information or concept.
+3. Short responses are needed 1-2 sentences.
 4. Avoid redundancy and unnecessary information.
 5. Use bullet points or numbered lists to organize information.
 6. Include key terms or definitions on the front of the flashcards.
-7. Use visuals or diagrams to enhance understanding.
+7. Don't ask questions only state information on the back of the flashcard.
 8. Keep the flashcards concise and to the point.
-9. Review and revise the flashcards regularly for better retention.
+9. If a prompt is broad give a brief overview of the topic.
+10. Ensure that each flash card focuses on a piece of information or concept. 
 
 Output in the following JSON format:
 {
@@ -27,13 +26,15 @@ Output in the following JSON format:
 }
 `;
 
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstructions: systemInstruction,
+});
+
 export async function POST() {
     try {
-        // Generate content using the model
-        const result = await model.generateContent("France");
+        const result = await model.generateContent("French Revolution");
         const text = await result.response.text();
-
-        // Create the response object with flashcard data
         const response = {
             flashcards: {
                 front: "France",
@@ -41,7 +42,6 @@ export async function POST() {
             },
         };
 
-        // Return the response as JSON
         return NextResponse.json(response);
     } catch (error) {
         console.error("Error generating content:", error);
