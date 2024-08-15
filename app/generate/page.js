d983d764aee4338382206b47e6da2fdfc70b106e
Francisco <figueroaf@moravian.edu>
2024-08-14 23:07:52 -0400

    started working on generating cards

---
+++ app/generate/page.js
@@ -0,0 +1,27 @@
+'use client'
+
+import { useUser } from "@clerk/nextjs"
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+export default function Generate() {
+    const { isLoaded, isSignedIn, user } = useUser()
+    const [flashcards, setFlashcards] = useState([])
+    const [flipped, setFlipped] = useState([])
+    const [text, setText] = useState('')
+    const [name, setName] = useState('')
+    const [open, setOpen] = useState(false)
+    const router = useRouter()
+
+    const handleSubmit = async () => {
+        fetch('api/gennerate', {
+            method: 'POST',
+            body: text,
+        }).then((res) => res.json()).then((data) > setFlashcards(data))
+    }
+
+
+
+
+
+}
